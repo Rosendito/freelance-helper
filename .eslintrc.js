ecffd9946150040e9aa0718c32ec3bf30a68890c
rosendito <rosendojose0617@gmail.com>
2022-12-25 13:22:02 -0400

    Ignore some eslint rules and files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,11 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
   rules: {
     // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    'vue/multi-word-component-names': 'off',
+    'vue/require-default-prop': 'off',
+    'no-func-assign': 'off',
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    'no-prototype-builtins': 'off'
   },
 }
